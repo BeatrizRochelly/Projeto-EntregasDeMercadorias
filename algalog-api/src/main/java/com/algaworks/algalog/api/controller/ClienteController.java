b066413d24cc07f37e1b9f53332604487bec8559
aluno <aluno@labdescomplica.local>
2023-03-15 23:17:54 -0300

    Organizando o projeto - cliente

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,14 +1,12 @@
 package com.algaworks.algalog.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
- 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ClienteController {
 	
-	 
-	
-	@GetMapping
+	@GetMapping ("/clientes")
 	public String listar() {
 		return "Teste";
 	}
